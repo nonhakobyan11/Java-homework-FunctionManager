c87a8cdc10aaa124ce3f21d7fc1a6c8fae9f80df
Nonna Hakobyan <nonhakobyan11@gmail.com>
2019-12-13 01:22:48 +0400

    Problem 1 and 2

---
+++ src/com/homework/FunctionManager.java
@@ -0,0 +1,13 @@
+package com.homework;
+
+public class FunctionManager {
+    // Problem 1
+    static int sum(int a, int b){
+        return a + b;
+    }
+
+    // Problem 2
+    static double mul(double a, double b){
+        return a * b;
+    }
+}
---
+++ src/com/homework/Main.java
@@ -0,0 +1,10 @@
+package com.homework;
+
+public class Main {
+
+    public static void main(String[] args) {
+        FunctionManager f = new FunctionManager();
+        System.out.println("Sum of two int: " + f.sum(3, 5));
+        System.out.println("Multiply of two double: " + f.mul(3, 5));
+    }
+}
